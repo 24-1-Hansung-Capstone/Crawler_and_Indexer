c9933295cec347aecd05be2a2a2a9697ea5f43e5
lee <sjlee9908@naver.com>
2024-05-07 18:58:49 +0900

    dd

--- NaverBlogCrawl/naverBlog_recursive_crawler.py
+++ NaverBlogCrawl/naverBlog_recursive_crawler.py
@@ -9,68 +9,67 @@ NAVER_CLIENT_SECRET = "HJq5tjv7HY"
 naverBlogCrawler = Naver_blog_crawler.NaverBlogCrawler(host="http://221.142.15.180:9200", authId ="elastic", authPw="elastic") #cAh+sWnbfRlXz1KimBp
 
 # 파일을 열고 'file'이라는 변수에 저장합니다.
-# with open('../searchWords.txt', 'r', encoding='utf-8') as file:
-#     # 파일 1줄 반복
-#     for line in file:
+with open('../searchWords.txt', 'r', encoding='utf-8') as file:
+    # 파일 1줄 반복
+    for line in file:
+
+        # 검색어 지정
+        encText = urllib.parse.quote(line.strip())
+        url = "https://openapi.naver.com/v1/search/blog?query=" + encText  # JSON 결과
+
+        # request
+        request = urllib.request.Request(url)
+        request.add_header("X-Naver-Client-Id", NAVER_CLIENT_ID)
+        request.add_header("X-Naver-Client-Secret", NAVER_CLIENT_SECRET)
+
+        # response 받기
+        response = urllib.request.urlopen(request)
+        rescode = response.getcode()
+
+        # 결과 파싱
+        if (rescode == 200):
+            response_body = response.read()
+            search_result = json.loads(response_body.decode('utf-8'))
+        else:
+            print("Error Code:" + rescode)
+            exit(rescode)
+
+        # 크롤링
+        i = 0
+        for item in search_result["items"]:
+            i += 1
+            naverBlogCrawler.title = item["title"]
+            print(naverBlogCrawler.crawl(item["link"], "blog",
+                                         ["div.se-main-container", "span.se-fs-", "span.se_publishDate"],
+                                         ["mainBody", "title", "date"], item))
+
+# url = "https://openapi.naver.com/v1/search/blog?query=" + "보문"  # JSON 결과
 #
-#         # 검색어 지정
-#         encText = urllib.parse.quote(line.strip())
-#         url = "https://openapi.naver.com/v1/search/blog?query=" + encText  # JSON 결과
+# # request
+# request = urllib.request.Request(url)
+# request.add_header("X-Naver-Client-Id", NAVER_CLIENT_ID)
+# request.add_header("X-Naver-Client-Secret", NAVER_CLIENT_SECRET)
 #
-#         # request
-#         request = urllib.request.Request(url)
-#         request.add_header("X-Naver-Client-Id", NAVER_CLIENT_ID)
-#         request.add_header("X-Naver-Client-Secret", NAVER_CLIENT_SECRET)
+# # response 받기
+# response = urllib.request.urlopen(request)
+# rescode = response.getcode()
 #
-#         # response 받기
-#         response = urllib.request.urlopen(request)
-#         rescode = response.getcode()
+# # 결과 파싱
+# if (rescode == 200):
+#     response_body = response.read()
+#     print(response_body.decode('utf-8'))
+#     search_result = json.loads(response_body.decode('utf-8'))
+# else:
+#     print("Error Code:" + rescode)
+#     exit(rescode)
 #
-#         # 결과 파싱
-#         print(response_body.decode('utf-8'))
-#         if (rescode == 200):
-#             response_body = response.read()
-#             search_result = json.loads(response_body.decode('utf-8'))
-#         else:
-#             print("Error Code:" + rescode)
-#             exit(rescode)
+# # 크롤링
+# i = 0
+# for item in search_result["items"]:
+#     i += 1
+#     naverBlogCrawler.title = item["title"]
+#     print(naverBlogCrawler.crawl(item["link"], "blog",
+#                                  ["div.se-main-container", "span.se-fs-", "span.se_publishDate"],
+#                                  ["mainBody", "title", "date"], item))
 #
-#         # 크롤링
-#         i = 0
-#         for item in search_result["items"]:
-#             i += 1
-#             naverBlogCrawler.title = item["title"]
-#             print(naverBlogCrawler.crawl(item["link"], "blog",
-#                                          ["div.se-main-container", "span.se-fs-", "span.se_publishDate"],
-#                                          ["mainBody", "title", "date"], item))
-
-url = "https://openapi.naver.com/v1/search/blog?query=" + "보문"  # JSON 결과
-
-# request
-request = urllib.request.Request(url)
-request.add_header("X-Naver-Client-Id", NAVER_CLIENT_ID)
-request.add_header("X-Naver-Client-Secret", NAVER_CLIENT_SECRET)
-
-# response 받기
-response = urllib.request.urlopen(request)
-rescode = response.getcode()
-
-# 결과 파싱
-if (rescode == 200):
-    response_body = response.read()
-    print(response_body.decode('utf-8'))
-    search_result = json.loads(response_body.decode('utf-8'))
-else:
-    print("Error Code:" + rescode)
-    exit(rescode)
-
-# 크롤링
-i = 0
-for item in search_result["items"]:
-    i += 1
-    naverBlogCrawler.title = item["title"]
-    print(naverBlogCrawler.crawl(item["link"], "blog",
-                                 ["div.se-main-container", "span.se-fs-", "span.se_publishDate"],
-                                 ["mainBody", "title", "date"], item))
-
-
+#
